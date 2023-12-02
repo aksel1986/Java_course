3443f22b2c5f0d51b0b4d6c157cedd3f1754c9eb
Aksel Mnatsakanyan <152707509+aksel1986@users.noreply.github.com>
2023-12-02 18:51:56 +0400

    My first git committtttt

---
+++ Lessons1/src/Lesson1.java
@@ -0,0 +1,47 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.print("Barev\n");
+        System.out.println("Lesson1");
+
+        byte x = 127;
+        System.out.println(x);
+
+        short y = -4542;
+        System.out.println(y);
+
+        int a = 455685;
+        System.out.println(a);
+
+        long b = 655858855995L;
+        System.out.println(b);
+
+        float aa = 45.4f;
+        double bb = 45.4;
+
+        boolean v = true;
+        System.out.println(v);
+        v = false;
+        System.out.println(v);
+
+        char t = '$';
+        System.out.println(t);
+        char tt = '\u003F';
+        System.out.println(tt);
+
+        String s = "barev Lessons1";
+        System.out.println(s);
+
+        String firstName = "Aksel";
+        String lastName = "Mnatsakanyan";
+
+        System.out.println(firstName + " " + lastName);
+
+        int k = 95;
+        System.out.println("K = " + k);
+
+    }
+
+
+
+
+}
